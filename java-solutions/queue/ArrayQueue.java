8cf1cbb59e163a8604018dd7983131a9dda54b1e
Vergil <kolesov645@gmail.com>
2021-02-25 20:22:00 +0300

    Main.java added, contracts fixed

--- java-solutions/queue/ArrayQueue.java
+++ java-solutions/queue/ArrayQueue.java
@@ -1,19 +1,22 @@
 package queue;
 
 // Model:
-// [x_1, x_2, ..., x_n]
+// [q_1, q_2, ..., q_n]
 // n -- capacity of queue
 
 // Inv:
-// n >= 0 && forall i = 1, ..., n : x_i != null
+// n >= 0 && forall i = 1, ..., n : q[i] != null
+
+// Immutability(this):
+// this.n == this.n' && forall i = 1, ..., this.n' : this.q[i] == this.q'[i]
 
 public class ArrayQueue {
     private int n = 0, l = 0, r = -1;
     private Object[] a = new Object[2];
 
     // Pred: x != null
-    // Post: this.n == this.n' + 1 && this.x_n == x
-    //       && forall i = 1, ..., this.n' : this.x_i == this.x'_i
+    // Post: this.n == this.n' + 1 && this.q[n] == x
+    //       && forall i = 1, ..., this.n' : this.q[i] == this.q'[i]
     public void enqueue(Object x) {
         ensureCapacity(n + 1);
         r = r + 1 < a.length ? r + 1 : 0;
@@ -22,15 +25,14 @@ public class ArrayQueue {
     }
 
     // Pred: true
-    // Post: R == this.x'_1 && this.n == this.n'
-    //       && forall i = 1, ..., this.n' : this.x_i == this.x'_i
+    // Post: R == this.q'[1] && Immutability(this)
     public Object element() {
         return a[l];
     }
 
     // Pred: this.n > 0
-    // Post: R == this.x'_1 && this.n == this.n' - 1
-    //       && forall i = 2, ..., this.n' : this.x_(i - 1) == this.x'_i
+    // Post: R == this.q'[1] && this.n == this.n' - 1
+    //       && forall i = 2, ..., this.n' : this.q[i - 1] == this.q'[i]
     public Object dequeue() {
         Object tmp = a[l];
         a[l] = null;
@@ -40,15 +42,13 @@ public class ArrayQueue {
     }
 
     // Pred: true
-    // Post: R == this.n' && this.n == this.n'
-    //       && forall i = 1, ..., this.n' : this.x_i == this.x'_i
+    // Post: R == this.n' && Immutability(this)
     public int size() {
         return n;
     }
 
     // Pred: true
-    // Post: R == (this.n' == 0) && this.n == this.n'
-    //       && forall i = 1, ..., this.n' : this.x_i == this.x'_i
+    // Post: R == (this.n' == 0) && Immutability(this)
     public boolean isEmpty() {
         return n == 0;
     }
@@ -56,17 +56,8 @@ public class ArrayQueue {
     // Pred: true
     // Post: this.n == 0
     public void clear() {
-        if (r < l) {
-            for (int i = l; i < a.length; i++) {
-                a[i] = null;
-            }
-            for (int i = 0; i <= r; i++) {
-                a[i] = null;
-            }
-        } else {
-            for (int i = l; i <= r; i++) {
-                a[i] = null;
-            }
+        for (int i = 0; i < n; i++) {
+            a[(l + i) % a.length] = null;
         }
         l = 0;
         r = -1;
@@ -76,18 +67,8 @@ public class ArrayQueue {
     private void ensureCapacity(int capacity) {
         if (capacity > a.length) {
             Object[] tmp = new Object[capacity * 2];
-            int k = 0;
-            if (r < l) {
-                for (int i = l; i < a.length; i++) {
-                    tmp[k++] = a[i];
-                }
-                for (int i = 0; i <= r; i++) {
-                    tmp[k++] = a[i];
-                }
-            } else {
-                for (int i = l; i <= r; i++) {
-                    tmp[k++] = a[i];
-                }
+            for (int i = 0; i < n; i++) {
+                tmp[i] = a[(l + i) % a.length];
             }
             a = tmp;
             l = 0;
--- java-solutions/queue/ArrayQueueADT.java
+++ java-solutions/queue/ArrayQueueADT.java
@@ -1,19 +1,22 @@
 package queue;
 
 // Model:
-// [x_1, x_2, ..., x_n]
+// [q_1, q_2, ..., q_n]
 // n -- capacity of queue
 
 // Inv:
-// n >= 0 && forall i = 1, ..., n : x_i != null
+// n >= 0 && forall i = 1, ..., n : q[i] != null
+
+// Immutability(queue):
+// queue.n == queue.n' && forall i = 1, ..., queue.n' : queue.q[i] == queue.q'[i]
 
 public class ArrayQueueADT {
     private int n = 0, l = 0, r = -1;
     private Object[] a = new Object[2];
 
     // Pred: queue != null && x != null
-    // Post: queue.n == queue.n' + 1 && queue.x_n == x
-    //       && forall i = 1, ..., queue.n' : queue.x_i == queue.x'_i
+    // Post: queue.n == queue.n' + 1 && queue.q[n] == x
+    //       && forall i = 1, ..., queue.n' : queue.q[i] == queue.q'[i]
     public static void enqueue(ArrayQueueADT queue, Object x) {
         ensureCapacity(queue, queue.n + 1);
         queue.r = queue.r + 1 < queue.a.length ? queue.r + 1 : 0;
@@ -22,15 +25,14 @@ public class ArrayQueueADT {
     }
 
     // Pred: queue != null
-    // Post: R == queue.x'_1 && queue.n == queue.n'
-    //       && forall i = 1, ..., queue.n' : queue.x_i == queue.x'_i
+    // Post: R == queue.q'[1] && Immutability(queue)
     public static Object element(ArrayQueueADT queue) {
         return queue.a[queue.l];
     }
 
     // Pred: queue != null && queue.n > 0
-    // Post: R == queue.x'_1 && queue.n == queue.n' - 1
-    //       && forall i = 2, ..., queue.n' : queue.x_(i - 1) == queue.x'_i
+    // Post: R == queue.q'[1] && queue.n == queue.n' - 1
+    //       && forall i = 2, ..., queue.n' : queue.q[i - 1] == queue.q'[i]
     public static Object dequeue(ArrayQueueADT queue) {
         Object tmp = queue.a[queue.l];
         queue.a[queue.l] = null;
@@ -40,15 +42,13 @@ public class ArrayQueueADT {
     }
 
     // Pred: queue != null
-    // Post: R == queue.n' && queue.n == queue.n'
-    //       && forall i = 1, ..., queue.n' : queue.x_i == queue.x'_i
+    // Post: R == queue.n' && Immutability(queue)
     public static int size(ArrayQueueADT queue) {
         return queue.n;
     }
 
     // Pred: queue != null
-    // Post: R == (queue.n' == 0) && queue.n == queue.n'
-    //       && forall i = 1, ..., queue.n' : queue.x_i == queue.x'_i
+    // Post: R == (queue.n' == 0) && Immutability(queue)
     public static boolean isEmpty(ArrayQueueADT queue) {
         return queue.n == 0;
     }
@@ -56,17 +56,8 @@ public class ArrayQueueADT {
     // Pred: queue != null
     // Post: queue.n == 0
     public static void clear(ArrayQueueADT queue) {
-        if (queue.r < queue.l) {
-            for (int i = queue.l; i < queue.a.length; i++) {
-                queue.a[i] = null;
-            }
-            for (int i = 0; i <= queue.r; i++) {
-                queue.a[i] = null;
-            }
-        } else {
-            for (int i = queue.l; i <=queue.r; i++) {
-                queue.a[i] = null;
-            }
+        for (int i = 0; i < queue.n; i++) {
+            queue.a[(queue.l + i) % queue.a.length] = null;
         }
         queue.l = 0;
         queue.r = -1;
@@ -76,18 +67,8 @@ public class ArrayQueueADT {
     private static void ensureCapacity(ArrayQueueADT queue, int capacity) {
         if (capacity > queue.a.length) {
             Object[] tmp = new Object[capacity * 2];
-            int k = 0;
-            if (queue.r < queue.l) {
-                for (int i = queue.l; i < queue.a.length; i++) {
-                    tmp[k++] = queue.a[i];
-                }
-                for (int i = 0; i <= queue.r; i++) {
-                    tmp[k++] = queue.a[i];
-                }
-            } else {
-                for (int i = queue.l; i <= queue.r; i++) {
-                    tmp[k++] = queue.a[i];
-                }
+            for (int i = 0; i < queue.n; i++) {
+                tmp[i] = queue.a[(queue.l + i) % queue.a.length];
             }
             queue.a = tmp;
             queue.l = 0;
