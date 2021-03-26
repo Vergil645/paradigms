5c5391e7cf3f996c604c9f80876aa1cefe553a61
Georgiy Korneev <kgeorgiy@kgeorgiy.info>
2021-03-26 15:47:01 +0300

    Checked

--- javascript-solutions/functionalExpression.js
+++ javascript-solutions/functionalExpression.js
@@ -10,9 +10,8 @@ const addOperation = function (arity, func, ...operators) {
     }
     return func;
 }
-const addBinaryOperation = function (operator, func) {
-    return addOperation(2, (f, g) => (...args) => func(f(...args), g(...args)), operator);
-}
+const addBinaryOperation = (operator, func) =>
+    addOperation(2, (f, g) => (...args) => func(f(...args), g(...args)), operator)
 
 const cnst = value => () => value;
 const variable = (name) => (...args) => args[ARGUMENT_POSITION[name]];
@@ -25,16 +24,17 @@ const negate = addOperation(1, f => (...args) => -f(...args), "negate");
 const one = CONSTANTS["one"];
 const two = CONSTANTS["two"];
 
+// :NOTE: Упростить
 const madd = addOperation(3, (f, g, p) => (...args) => f(...args) * g(...args) + p(...args), "*+", "madd");
 const floor = addOperation(1, f => (...args) => Math.floor(f(...args)), "_", "floor");
 const ceil = addOperation(1, f => (...args) => Math.ceil(f(...args)), "^", "ceil");
 
-const parse = expression => {
+function parse(expression) {
     let stack = [];
     for (const token of expression.split(" ").filter(word => word !== "")) {
         if (token in OPERATIONS) {
             let op = OPERATIONS[token];
-            stack.push(op.func(...stack.splice(-op.arity, op.arity)));
+            stack.push(op.func(...stack.splice(-op.arity)));
         } else if (token in ARGUMENT_POSITION) {
             stack.push(variable(token));
         } else if (token in CONSTANTS) {
