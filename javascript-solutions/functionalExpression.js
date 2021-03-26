f60778d09bf19f5950627ef34d440133aacb2d6e
Vergil <kolesov645@gmail.com>
2021-03-26 15:31:20 +0300

    OneFP modification added

--- javascript-solutions/functionalExpression.js
+++ javascript-solutions/functionalExpression.js
@@ -4,16 +4,14 @@ const OPERATIONS = {}
 const ARGUMENT_POSITION = {"x": 0, "y": 1, "z": 2};
 const CONSTANTS = {"one": () => 1, "two": () => 2}
 
-const addOperation = function (operator, arity, func) {
-    OPERATIONS[operator] = {arity: arity, func: func};
+const addOperation = function (arity, func, ...operators) {
+    for (const operator of operators) {
+        OPERATIONS[operator] = {arity: arity, func: func};
+    }
     return func;
 }
-
-const foldLeft = function (init, f, ...args) {
-    for (const arg of args) {
-        init = f(init, arg);
-    }
-    return init;
+const addBinaryOperation = function (operator, func) {
+    return addOperation(2, (f, g) => (...args) => func(f(...args), g(...args)), operator);
 }
 
 const min = (x, y) => x < y ? x : y;
@@ -21,21 +19,18 @@ const max = (x, y) => x > y ? x : y;
 
 const cnst = value => () => value;
 const variable = (name) => (...args) => args[ARGUMENT_POSITION[name]];
-const add = addOperation("+", 2, (f, g) => (...args) => f(...args) + g(...args));
-const subtract = addOperation("-", 2, (f, g) => (...args) => f(...args) - g(...args));
-const multiply = addOperation("*", 2, (f, g) => (...args) => f(...args) * g(...args));
-const divide = addOperation("/", 2, (f, g) => (...args) => f(...args) / g(...args));
-const negate = addOperation("negate", 1, f => (...args) => -f(...args));
+const add = addBinaryOperation("+", (x, y) => x + y);
+const subtract = addBinaryOperation("-", (x, y) => x - y);
+const multiply = addBinaryOperation("*", (x, y) => x * y);
+const divide = addBinaryOperation("/", (x, y) => x / y);
+const negate = addOperation(1, f => (...args) => -f(...args), "negate");
 
 const one = CONSTANTS["one"];
 const two = CONSTANTS["two"];
 
-const min5 = addOperation("min5", 5,
-    (...functions) => (...args) => foldLeft(Infinity, (x, func) => min(x, func(...args)), ...functions)
-);
-const max3 = addOperation("max3", 3,
-    (...functions) => (...args) => foldLeft(-Infinity, (x, func) => max(x, func(...args)), ...functions)
-);
+const madd = addOperation(3, (f, g, p) => (...args) => f(...args) * g(...args) + p(...args), "*+", "madd");
+const floor = addOperation(1, f => (...args) => Math.floor(f(...args)), "_", "floor");
+const ceil = addOperation(1, f => (...args) => Math.ceil(f(...args)), "^", "ceil");
 
 const parse = expression => {
     let stack = [];
@@ -54,19 +49,19 @@ const parse = expression => {
     return stack.pop();
 }
 
-const testExpr = add(
-    subtract(
-        multiply(
-            variable('x'),
-            variable('x')
-        ),
-        multiply(
-            cnst(2),
-            variable('x')
-        )
-    ),
-    cnst(1)
-);
-for (let x = 0; x <= 10; x++) {
-    println(testExpr(x))
-}
+// const testExpr = add(
+//     subtract(
+//         multiply(
+//             variable('x'),
+//             variable('x')
+//         ),
+//         multiply(
+//             cnst(2),
+//             variable('x')
+//         )
+//     ),
+//     cnst(1)
+// );
+// for (let x = 0; x <= 10; x++) {
+//     println(testExpr(x))
+// }
