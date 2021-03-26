81716f3ea7572dc096f981319b5fd7998ae82ac9
Vergil <kolesov645@gmail.com>
2021-03-26 15:05:53 +0300

    OneTwo and OneMinMax modifications added

--- javascript-solutions/functionalExpression.js
+++ javascript-solutions/functionalExpression.js
@@ -1,50 +1,72 @@
 "use strict";
 
-const cnst = value => x => value;
-const variable = () => x => x;
-const add = (f, g) => x => f(x) + g(x);
-const subtract = (f, g) => x => f(x) - g(x);
-const multiply = (f, g) => x => f(x) * g(x);
-const divide = (f, g) => x => f(x) / g(x);
-const negate = f => x => -f(x);
+const OPERATIONS = {}
+const ARGUMENT_POSITION = {"x": 0, "y": 1, "z": 2};
+const CONSTANTS = {"one": () => 1, "two": () => 2}
 
-const parse = expression => {
-    let i = expression.length - 1;
-
-    const parseConst = () => {
-        let value = 0, e = 1;
-        while (i >= 0 && '0' <= expression[i] && expression[i] <= '9') {
-            value += expression[i] * e;
-            e *= 10;
-            i--;
-        }
-        if (i >= 0 && expression[i] === '-') {
-            value = -value;
-            i--;
-        }
-        return cnst(value);
-    }
-
-    const parseImpl = () => {
-        while (i >= 0 && expression[i] === ' ') {
-            i--;
-        }
-        if ('0' <= expression[i] && expression[i] <= '9') {
-            return parseConst()
-        } else if (expression[i] === 'x') {
-            i--;
-            return variable();
-        } else {
-            let operand = expression[i--];
-            let g = parseImpl(), f = parseImpl();
-
-            if (operand === '+') return add(f, g);
-            else if (operand === '-') return subtract(f, g);
-            else if (operand === '*') return multiply(f, g);
-            else if (operand === '/') return divide(f, g);
-        }
-    }
-    return parseImpl()
+const addOperation = function (operator, arity, func) {
+    OPERATIONS[operator] = {arity: arity, func: func};
+    return func;
 }
 
-//println("Hello!")
+const foldLeft = function (init, f, ...args) {
+    for (const arg of args) {
+        init = f(init, arg);
+    }
+    return init;
+}
+
+const min = (x, y) => x < y ? x : y;
+const max = (x, y) => x > y ? x : y;
+
+const cnst = value => () => value;
+const variable = (name) => (...args) => args[ARGUMENT_POSITION[name]];
+const add = addOperation("+", 2, (f, g) => (...args) => f(...args) + g(...args));
+const subtract = addOperation("-", 2, (f, g) => (...args) => f(...args) - g(...args));
+const multiply = addOperation("*", 2, (f, g) => (...args) => f(...args) * g(...args));
+const divide = addOperation("/", 2, (f, g) => (...args) => f(...args) / g(...args));
+const negate = addOperation("negate", 1, f => (...args) => -f(...args));
+
+const one = CONSTANTS["one"];
+const two = CONSTANTS["two"];
+
+const min5 = addOperation("min5", 5,
+    (...functions) => (...args) => foldLeft(Infinity, (x, func) => min(x, func(...args)), ...functions)
+);
+const max3 = addOperation("max3", 3,
+    (...functions) => (...args) => foldLeft(-Infinity, (x, func) => max(x, func(...args)), ...functions)
+);
+
+const parse = expression => {
+    let stack = [];
+    for (const token of expression.split(" ").filter(word => word !== "")) {
+        if (token in OPERATIONS) {
+            let op = OPERATIONS[token];
+            stack.push(op.func(...stack.splice(-op.arity, op.arity)));
+        } else if (token in ARGUMENT_POSITION) {
+            stack.push(variable(token));
+        } else if (token in CONSTANTS) {
+            stack.push(CONSTANTS[token]);
+        } else {
+            stack.push(cnst(+token));
+        }
+    }
+    return stack.pop();
+}
+
+const testExpr = add(
+    subtract(
+        multiply(
+            variable('x'),
+            variable('x')
+        ),
+        multiply(
+            cnst(2),
+            variable('x')
+        )
+    ),
+    cnst(1)
+);
+for (let x = 0; x <= 10; x++) {
+    println(testExpr(x))
+}
