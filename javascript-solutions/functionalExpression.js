2a51545ba3b6b6f901b6c1f46d8d5510c1c44d4b
Vergil <kolesov645@gmail.com>
2021-03-30 23:18:23 +0300

    v0.90

--- javascript-solutions/functionalExpression.js
+++ javascript-solutions/functionalExpression.js
@@ -1,38 +1,45 @@
 "use strict";
 
 const OPERATIONS = {};
+const CONSTANTS = {};
 const ARGUMENT_POSITION = {"x": 0, "y": 1, "z": 2};
-const CONSTANTS = {"one": () => 1, "two": () => 2};
 
-const addOperation = function (arity, func, ...operators) {
-    for (const operator of operators) {
+function addOperation(arity, calc, ...operators) {
+    const func = (...expressions) => (...args) => calc(...expressions.map(expr => expr(...args)));
+    for (const operator of operators)
         OPERATIONS[operator] = {arity: arity, func: func};
-    }
+    return func;
+}
+function addConstant(value, ...names) {
+    const func = () => value;
+    for (const name of names)
+        CONSTANTS[name] = func;
     return func;
 }
 
-const operation = calc => (...functions) => (...args) => calc(...functions.map(f => f(...args)));
-
 const cnst = value => () => value;
-const variable = (name) => (...args) => args[ARGUMENT_POSITION[name]];
+const variable = (name) => {
+    const arg_pos = ARGUMENT_POSITION[name];
+    return (...args) => args[arg_pos];
+}
 
-const add = addOperation(2, operation((x, y) => x + y), "+");
-const subtract = addOperation(2, operation((x, y) => x - y), "-");
-const multiply = addOperation(2, operation((x, y) => x * y), "*");
-const divide = addOperation(2, operation((x, y) => x / y), "/");
-const negate = addOperation(1, operation(x => -x), "negate");
+const add = addOperation(2, (x, y) => x + y, "+");
+const subtract = addOperation(2, (x, y) => x - y, "-");
+const multiply = addOperation(2, (x, y) => x * y, "*");
+const divide = addOperation(2, (x, y) => x / y, "/");
+const negate = addOperation(1, x => -x, "negate");
 
-const one = CONSTANTS["one"];
-const two = CONSTANTS["two"];
-const madd = addOperation(3, operation((x, y, z) => x * y + z), "*+", "madd");
-const floor = addOperation(1, operation(Math.floor), "_", "floor");
-const ceil = addOperation(1, operation(Math.ceil), "^", "ceil");
+const one = addConstant(1, "one");
+const two = addConstant(2, "two");
+const madd = addOperation(3, (x, y, z) => x * y + z, "*+", "madd");
+const floor = addOperation(1, Math.floor, "_", "floor");
+const ceil = addOperation(1, Math.ceil, "^", "ceil");
 
 function parse(expression) {
-    let stack = [];
+    const stack = [];
     for (const token of expression.split(" ").filter(word => word !== "")) {
         if (token in OPERATIONS) {
-            let op = OPERATIONS[token];
+            const op = OPERATIONS[token];
             stack.push(op.func(...stack.splice(-op.arity)));
         } else if (token in ARGUMENT_POSITION) {
             stack.push(variable(token));
---
+++ javascript-solutions/objectExpression.js
@@ -0,0 +1,164 @@
+"use strict";
+
+const ARGUMENT_POSITION = {"x": 0, "y": 1, "z": 2};
+const CONSTANTS = {};
+const OPERATIONS = {
+    "+": {arity: 2, constructor: Add},
+    "-": {arity: 2, constructor: Subtract},
+    "*": {arity: 2, constructor: Multiply},
+    "/": {arity: 2, constructor: Divide},
+    "negate": {arity: 1, constructor: Negate}
+};
+
+
+function Const(value) { this.value = value; }
+Const.prototype.constructor = Const;
+Const.prototype.evaluate = function () { return this.value; }
+Const.prototype.diff = () => new Const(0);
+Const.prototype.simplify = function () { return new Const(this.value); }
+Const.prototype.toString = function () { return this.value.toString(); }
+Const.equals = (expression, value) => {
+    return expression.constructor === Const && (value === undefined || expression.evaluate() === value);
+}
+
+
+function Variable(name) {
+    this.arg_pos = ARGUMENT_POSITION[name];
+    this.name = name;
+}
+Variable.prototype.constructor = Variable;
+Variable.prototype.evaluate = function (...args) { return args[this.arg_pos]; }
+Variable.prototype.diff = function (var_name) { return new Const(this.name === var_name ? 1 : 0); }
+Variable.prototype.simplify = function () { return new Variable(this.name); }
+Variable.prototype.toString = function () { return this.name; }
+
+
+function AbstractOperation(...expressions) { this.expressions = expressions; }
+AbstractOperation.prototype.constructor = AbstractOperation;
+AbstractOperation.prototype.evaluateImpl = () => undefined;
+AbstractOperation.prototype.evaluate = function (...args) {
+    return this.evaluateImpl(...this.expressions.map(expr => expr.evaluate(...args)));
+}
+AbstractOperation.prototype.diffImpl = () => undefined;
+AbstractOperation.prototype.diff = function (var_name) {
+    return this.diffImpl(...this.expressions.map(expr => expr.diff(var_name)));
+}
+AbstractOperation.prototype.simplifyImpl = () => undefined;
+AbstractOperation.prototype.simplify = function () {
+    let simples = this.expressions.map(expr => expr.simplify());
+    for (let simple of simples) {
+        if (!Const.equals(simple))
+            return this.simplifyImpl(...simples);
+    }
+    return new Const(this.evaluateImpl(...simples.map(expr => expr.evaluate())));
+}
+AbstractOperation.prototype.operator = undefined;
+AbstractOperation.prototype.toString = function () {
+    return String.prototype.concat(...this.expressions.map(expr => `${expr.toString()} `), this.operator);
+}
+
+
+// function Generator(arity) {
+//     let res = function() {};
+//     res.prototype = Object.create(AbstractOperation.prototype);
+//     res.prototype.arity = arity;
+//     return res;
+// }
+
+
+function Add(f, g) { AbstractOperation.call(this, f, g); }
+Add.prototype = Object.create(AbstractOperation.prototype);
+Add.prototype.constructor = Add;
+Add.prototype.evaluateImpl = (x, y) => x + y;
+Add.prototype.diffImpl = (diff_0, diff_1) => new Add(diff_0, diff_1);
+Add.prototype.simplifyImpl = (simple_0, simple_1) => {
+    if (Const.equals(simple_0, 0)) return simple_1;
+    if (Const.equals(simple_1, 0)) return simple_0;
+    return new Add(simple_0, simple_1);
+}
+Add.prototype.operator = "+";
+
+
+function Subtract(f, g) { AbstractOperation.call(this, f, g); }
+Subtract.prototype = Object.create(AbstractOperation.prototype);
+Subtract.prototype.constructor = Subtract;
+Subtract.prototype.evaluateImpl = (x, y) => x - y;
+Subtract.prototype.diffImpl = (diff_0, diff_1) => new Subtract(diff_0, diff_1);
+Subtract.prototype.simplifyImpl = (simple_0, simple_1) => {
+    if (Const.equals(simple_0, 0)) return new Negate(simple_1);
+    if (Const.equals(simple_1, 0)) return simple_0;
+    return new Subtract(simple_0, simple_1);
+}
+Subtract.prototype.operator = "-";
+Subtract.prototype.arity = 2;
+
+
+function Multiply(f, g) { AbstractOperation.call(this, f, g); }
+Multiply.prototype = Object.create(AbstractOperation.prototype);
+Multiply.prototype.constructor = Multiply;
+Multiply.prototype.evaluateImpl = (x, y) => x * y;
+Multiply.prototype.diffImpl = function (diff_0, diff_1) {
+    return new Add(
+        new Multiply(this.expressions[0], diff_1),
+        new Multiply(diff_0, this.expressions[1])
+    );
+}
+Multiply.prototype.simplifyImpl = (simple_0, simple_1) => {
+    if (Const.equals(simple_0, 0) || Const.equals(simple_1, 0)) return new Const(0);
+    if (Const.equals(simple_0, 1)) return simple_1;
+    if (Const.equals(simple_1, 1)) return simple_0;
+    if (Const.equals(simple_0, -1)) return new Negate(simple_1);
+    if (Const.equals(simple_1, -1)) return new Negate(simple_0);
+    return new Multiply(simple_0, simple_1);
+}
+Multiply.prototype.operator = "*";
+Multiply.prototype.arity = 2;
+
+
+function Divide(f, g) { AbstractOperation.call(this, f, g); }
+Divide.prototype = Object.create(AbstractOperation.prototype);
+Divide.prototype.constructor = Divide;
+Divide.prototype.evaluateImpl = (x, y) => x / y;
+Divide.prototype.diffImpl = function (diff_0, diff_1) {
+    return new Divide(
+        new Subtract(
+            new Multiply(diff_0, this.expressions[1]),
+            new Multiply(this.expressions[0], diff_1)
+        ),
+        new Multiply(this.expressions[1], this.expressions[1])
+    );
+}
+Divide.prototype.simplifyImpl = (simple_0, simple_1) => {
+    if (Const.equals(simple_0, 0)) return new Const(0);
+    if (Const.equals(simple_1, 1)) return simple_0;
+    if (Const.equals(simple_1, -1)) return new Negate(simple_0);
+    return new Divide(simple_0, simple_1);
+}
+Divide.prototype.operator = "/";
+
+
+function Negate(f) { AbstractOperation.call(this, f); }
+Negate.prototype = Object.create(AbstractOperation.prototype);
+Negate.prototype.constructor = Negate;
+Negate.prototype.evaluateImpl = (x) => -x;
+Negate.prototype.diffImpl = (diff_0) => new Negate(diff_0);
+Negate.prototype.simplifyImpl = (simple_0) => new Negate(simple_0);
+Negate.prototype.operator = "negate";
+
+
+function parse(expression) {
+    const stack = [];
+    for (const token of expression.split(" ").filter(word => word !== "")) {
+        if (token in OPERATIONS) {
+            const op = OPERATIONS[token];
+            stack.push(new op.constructor(...stack.splice(-op.arity)));
+        } else if (token in ARGUMENT_POSITION) {
+            stack.push(new Variable(token));
+        } else if (token in CONSTANTS) {
+            stack.push(new CONSTANTS[token]);
+        } else {
+            stack.push(new Const(+token));
+        }
+    }
+    return stack.pop();
+}
