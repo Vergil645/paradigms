711f9670dd87267ffeed27dbd477df05fe8d7038
Vergil <kolesov645@gmail.com>
2021-03-31 15:51:56 +0300

    Mod added

--- javascript-solutions/objectExpression.js
+++ javascript-solutions/objectExpression.js
@@ -148,20 +148,30 @@ Negate.prototype.simplifyImpl = (simple_0) => new Negate(simple_0);
 function Hypot(f, g) { AbstractOperation.call(this, f, g); }
 Hypot.prototype = OperationFactory(Hypot, "hypot");
 Hypot.prototype.evaluateImpl = (x, y) => x * x + y * y;
-Hypot.prototype.diffImpl = (diff_0, diff_1) => {
+Hypot.prototype.diff = function (var_name) {
     return new Add(
-        new Multiply(
-            new Multiply(new Const(2), this.expressions[0]),
-            diff_0
-        ),
-        new Multiply(
-            new Multiply(new Const(2), this.expressions[1]),
-            diff_1
-        )
-    );
+        new Multiply(this.expressions[0], this.expressions[0]),
+        new Multiply(this.expressions[1], this.expressions[1])
+    ).diff(var_name);
 }
 Hypot.prototype.simplifyImpl = (simple_0, simple_1) => {
     if (Const.equals(simple_0, 0)) return new Multiply(simple_1, simple_1);
     if (Const.equals(simple_1, 0)) return new Multiply(simple_0, simple_0);
-    return new Add(simple_0, simple_1);
+    return new Hypot(simple_0, simple_1);
+}
+
+function HMean(f, g) { AbstractOperation.call(this, f, g); }
+HMean.prototype = OperationFactory(HMean, "hmean");
+HMean.prototype.evaluateImpl = (x, y) => 2 / (1 / x + 1 / y);
+HMean.prototype.diff = function (var_name) {
+    return new Divide(
+        new Const(2),
+        new Add(
+            new Divide(new Const(1), this.expressions[0]),
+            new Divide(new Const(1), this.expressions[1])
+        )
+    ).diff(var_name);
+}
+HMean.prototype.simplifyImpl = (simple_0, simple_1) => {
+    return undefined;
 }
