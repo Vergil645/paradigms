eaa5317fe892e8500888db4734e9e3e284b5ee13
Vergil <kolesov645@gmail.com>
2021-04-22 21:45:11 +0300

    v1 (with Tensor)

--- clojure-solutions/linear.clj
+++ clojure-solutions/linear.clj
@@ -7,28 +7,8 @@
     (apply mapv f vs)))
 (defn vec-nums [f]
   (fn [v & nums]
-    {:pre [(and (vecs? v) (every? number? nums))]}
+    {:pre [(vecs? v) (every? number? nums)]}
     (mapv #(apply f % nums) v)))
-
-
-;Matrix-utility
-(defn matrix? [m] (and (vector? m) (apply vecs? m)))
-(defn m-height [m] (count m))
-(defn m-width [m] (count (nth m 0)))
-(defn m-ms [f]
-  (fn [& ms]
-    {:pre [(and (every? matrix? ms) (apply == (mapv m-height ms)) (apply == (mapv m-width ms)))]}
-    (apply mapv f ms)))
-(defn m-vec [f]
-  (fn [m vec]
-    {:pre [(and (matrix? m) (vecs? vec))]}
-    (mapv #(f % vec) m)))
-(defn m-nums [f]
-  (fn [m & nums]
-    {:pre [(and (matrix? m) (every? number? nums))]}
-    (let [vec-op (vec-nums f)]
-      (mapv #(apply vec-op % nums) m))))
-
 
 ;Vectors
 (def v+ (vec-vecs +))
@@ -39,12 +19,29 @@
   {:pre [(apply vecs? vs)]}
   (apply + (apply v* vs)))
 (defn vect [& vs]
-  {:pre [(and (apply vecs? vs) (apply == 3 (mapv count vs)))]}
+  {:pre [(apply vecs? vs) (apply == 3 (mapv count vs))]}
   (letfn [(minor [va vb i j]
-            (- (* (nth va i) (nth vb j)) (* (nth va j) (nth vb i))))]
+            (- (* (va i) (vb j)) (* (va j) (vb i))))]
     (reduce #(vector (minor %1 %2 1 2) (minor %1 %2 2 0) (minor %1 %2 0 1)) vs)))
 (def v*s (vec-nums *))
 
+
+;Matrix-utility
+(defn matrix? [m] (and (vector? m) (apply vecs? m)))
+(defn m-height [m] (count m))
+(defn m-width [m] (count (m 0)))
+(defn m-ms [f]
+  (fn [& ms]
+    {:pre [(every? matrix? ms) (apply == (mapv m-height ms)) (apply == (mapv m-width ms))]}
+    (apply mapv f ms)))
+(defn m-vec [f]
+  (fn [m vec]
+    {:pre [(matrix? m) (vecs? vec)]}
+    (mapv #(f % vec) m)))
+(defn m-nums [f]
+  (fn [m & nums]
+    {:pre [(matrix? m) (every? number? nums)]}
+    (mapv (fn [mx] (mapv #(apply f % nums) mx)) m)))
 
 ;Matrices
 (def m+ (m-ms v+))
@@ -57,7 +54,22 @@
   {:pre [(matrix? m)]}
   (reduce #(mapv conj %1 %2) (vec (repeat (m-width m) [])) m))
 (defn m*m [& ms]
-  (letfn [(m*m-bin [ma mb]
-            {:pre [(and (matrix? ma) (matrix? mb) (== (m-width ma) (m-height mb)))]}
-            (let [bt (transpose mb)] (mapv #(m*v bt %) ma)))]
-    (reduce m*m-bin ms)))
+  {:pre [(first (reduce
+                  (fn [[res ma] mb]
+                    (vector (and res (matrix? ma) (matrix? mb) (== (m-width ma) (m-height mb))) mb))
+                  [true (first ms)] (rest ms)))]}
+  (reduce (fn [ma mb] (let [bt (transpose mb)] (mapv #(m*v bt %) ma))) ms))
+
+
+;Tensor-utility
+(defn t-ts [f]
+  (letfn [(t-op [ta tb]
+            (if (number? ta)
+              (f ta tb)
+              (mapv t-op ta tb)))] t-op))
+
+;Tensors
+(def t+ (t-ts +))
+(def t- (t-ts -))
+(def t* (t-ts *))
+(def td (t-ts /))
