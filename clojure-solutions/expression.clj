365e2ddcbd01c382c2bb5e5b97f7ada8c9cff3f0
Vergil <kolesov645@gmail.com>
2021-04-30 14:47:20 +0300

    expression.clj improved

--- clojure-solutions/expression.clj
+++ clojure-solutions/expression.clj
@@ -2,9 +2,6 @@
 (defn create-operation [func]
   (fn [& expressions]
     (fn [args-map] (apply func (map #(% args-map) expressions)))))
-(defn create-unary-operation [func]
-  (fn [expr]
-    (fn [args-map] (func (expr args-map)))))
 
 
 ;;Operations
@@ -19,19 +16,21 @@
 (defn variable [name]
   (fn [args-map] (args-map name)))
 
-(def negate (create-unary-operation -))
 (def add (create-operation +))
 (def subtract (create-operation -))
 (def multiply (create-operation *))
 (def divide (create-operation _div))
+(def negate subtract)
 
 
 ;;Parser
+(def variable-names #{'x, 'y, 'z})
 (def operators-map {'+ add, '- subtract, '* multiply, '/ divide, 'negate negate})
+(defn parse-lexeme [lexeme]
+  (cond
+    (number? lexeme) (constant lexeme)
+    (contains? variable-names lexeme) (variable (str (variable-names lexeme)))
+    (list? lexeme) (apply (operators-map (first lexeme)) (mapv parse-lexeme (rest lexeme)))
+    :else nil))
 (defn parseFunction [expr]
-  (letfn [(parse [lexeme]
-            (cond
-              (list? lexeme) (apply (operators-map (first lexeme)) (mapv parse (rest lexeme)))
-              (number? lexeme) (constant lexeme)
-              :else (variable (str lexeme))))]
-    (parse (read-string expr))))
+  (parse-lexeme (read-string expr)))
