2e328ac1fc74af3e129d8a6671bcec44984fc252
Vergil <kolesov645@gmail.com>
2021-05-21 14:21:55 +0300

    Hw-12 Gcd added

--- prolog-solutions/primes.pl
+++ prolog-solutions/primes.pl
@@ -22,19 +22,10 @@ prime_divisors(N, Divisors) :- nonvar(Divisors), !, make_number(N, 1, Divisors),
 make_number(N, N, []).
 make_number(N, R, [X | Tail]) :- R1 is R * X, make_number(N, R1, Tail).
 
-% Power divisors (N is number)
-power_divisors(N, I, D) :- number(N), !, make_power_divisors(N, I, 2, D).
-make_power_divisors(1, _, _, []).
-make_power_divisors(N, I, X, D) :- X =< N, prime(N), rep(N, I, D), !.
-make_power_divisors(N, I, X, D) :- 0 is mod(N, X), N1 is N / X, rep(X, I, D1), !, make_power_divisors(N1, I, X, D2), append(D1, D2, D).
-make_power_divisors(N, I, X, D) :- X < N, X1 is X + 1, make_power_divisors(N, I, X1, D).
-rep(N, 0, []).
-rep(N, I, [N | Tail]) :- I1 is I - 1, rep(N, I1, Tail).
-
-% Power divisors (N is variable)
-power_divisors(N, I, D) :- var(N), !, make_power_number(N, I, 1, D), power_divisors(N, I, D).
-make_power_number(N, _, N, []).
-make_power_number(N, I, R, [X | Tail]) :- R1 is R * X, skip([X | Tail], I, D), make_power_number(N, I, R1, D).
-skip([], _, []).
-skip(D, 0, D).
-skip([X | Tail], I, D) :- I1 is I - 1, skip(Tail, I1, D).
+% GCD
+gcd(A, B, GCD) :- prime_divisors(A, DA), prime_divisors(B, DB), lists_intersection(DA, DB, L), make_number(GCD, 1, L), !.
+lists_intersection([], _, []) :- !.
+lists_intersection(_, [], []) :- !.
+lists_intersection([X | TX], [X | TY], [X | T]) :- lists_intersection(TX, TY, T), !.
+lists_intersection([X | TX], [Y | TY], L) :- X > Y, !, lists_intersection([X | TX], TY, L).
+lists_intersection([X | TX], [Y | TY], L) :- lists_intersection(TX, [Y | TY], L), !.
