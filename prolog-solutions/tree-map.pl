74540698053a2b037d051e470a49447c7955996c
Vergil <kolesov645@gmail.com>
2021-05-27 20:36:08 +0300

    Tree of Decart

--- prolog-solutions/tree-map.pl
+++ prolog-solutions/tree-map.pl
@@ -1,170 +1,85 @@
-% Secondary methods
-sorted([]).
-sorted([(_, _)]).
-sorted([(K1, _), (K2, _) | Tail]) :- K1 =< K2.
+% Create node
+create_node(K, V, Node) :-
+	rand_int(1000000007, P),
+	Node = node(K, V, P, null, null).
 
 
-% Node methods
-get_max(leaf(K, _), K).
-get_max(node2(Max, _, _, _), Max).
-get_max(node3(Max, _, _, _, _, _), Max).
+% Split
+split(null, _, null, null).
 
-create_node(Son1, null, null, Son1).
-
-create_node(Son1, Son2, null, node2(Max, Key1, Son1, Son2)) :-
-	get_max(Son2, Max),
-	get_max(Son1, Key1).
-
-create_node(Son1, Son2, Son3, node3(Max, Key1, Key2, Son1, Son2, Son3)) :-
-	get_max(Son3, Max),
-	get_max(Son1, Key1),
-	get_max(Son2, Key2).
-
-create_node(Son1, Son2, Son3, null, Node1, null) :- 
-	!, create_node(Son1, Son2, Son3, Node1).
-
-create_node(Son1, Son2, null, Son4, Node1, null) :- 
-	!, create_node(Son1, Son2, Son4, Node1).
-
-create_node(Son1, null, Son3, Son4, Node1, null) :- 
-	!, create_node(Son1, Son3, Son4, Node1).
-
-create_node(Son1, Son2, Son3, Son4, Node1, Node2) :-
-	create_node(Son1, Son2, null, Node1),
-	create_node(Son3, Son4, null, Node2).
-
-
-% TreeMap constructors
-map_build([], null) :- !.
-
-map_build(ListMap, TreeMap) :- 
-	sorted(ListMap), 
-	!, build_leafs(ListMap, LeafsList), 
-	build_tree(LeafsList, TreeMap).
-
-build_leafs([], []).
-
-build_leafs([(Key, Value) | T1], [leaf(Key, Value) | T2]) :- 
-	build_leafs(T1, T2).
-
-build_tree([Node], Node).
-
-build_tree(NodesList, TreeMap) :- 
-	build_nodes(NodesList, NextNodesList), 
-	build_tree(NextNodesList, TreeMap).
-
-build_nodes([], []).
-build_nodes([Son1, Son2, Son3], [Node]) :- 
-	!, create_node(Son1, Son2, Son3, Node).
+split(node(K, V, P, L, R), X, Node1, Node2) :-
+	K =< X,
+	!, split(R, X, NewR, Node2),
+	Node1 = node(K, V, P, L, NewR).
 	
-build_nodes([Son1, Son2 | T1], [Node | T2]) :- 
-	create_node(Son1, Son2, null, Node), 
-	build_nodes(T1, T2).
+split(node(K, V, P, L, R), X, Node1, Node2) :-
+	split(L, X, Node1, NewL),
+	Node2 = node(K, V, P, NewL, R).
+
+
+% Merge
+merge(T1, null, T1) :- !.
+merge(null, T2, T2).
+
+merge(node(K1, V1, P1, L1, R1), node(K2, V2, P2, L2, R2), Node) :-
+	P1 > P2, 
+	!, merge(R1, node(K2, V2, P2, L2, R2), NewR1),
+	Node = node(K1, V1, P1, L1, NewR1).
+
+merge(node(K1, V1, P1, L1, R1), node(K2, V2, P2, L2, R2), Node) :-
+	merge(node(K1, V1, P1, L1, R1), L2, NewL2),
+	Node = node(K2, V2, P2, NewL2, R2).
 
 
 % Map get
-map_get(leaf(K, V), Key, Value) :- 
-	Key == K, 
-	Value = V.
+map_get(node(Key, Value, _, _, _), Key, Value).
 
-map_get(node2(_, Key1, _, Son2), Key, Value) :- 
-	Key1 < Key, !, 
-	map_get(Son2, Key, Value).
-	
-map_get(node2(_, _, Son1, _), Key, Value) :- 
-	map_get(Son1, Key, Value).
+map_get(node(K, V, _, L, _), Key, Value) :-
+	Key < K, 
+	!, map_get(L, Key, Value).
 
-map_get(node3(_, _, Key2, _, _, Son3), Key, Value) :- 
-	Key2 < Key, !, 
-	map_get(Son3, Key, Value).
-	
-map_get(node3(_, Key1, _, _, Son2, _), Key, Value) :- 
-	Key1 < Key, !, 
-	map_get(Son2, Key, Value).
-	
-map_get(node3(_, _, _, Son1, _, _), Key, Value) :- 
-	map_get(Son1, Key, Value).
+map_get(node(K, V, _, _, R), Key, Value) :-
+	map_get(R, Key, Value).
 
 
 % Map put
 map_put(TreeMap, Key, Value, Result) :- 
 	map_get(TreeMap, Key, _), 
-	!, tree_set(TreeMap, Key, Value, Result).
+	!, set_node(TreeMap, Key, Value, Result).
 
-map_put([], Key, Value, leaf(Key, Value)) :- !.
+map_put(TreeMap, Key, Value, Result) :-
+	create_node(Key, Value, Node), 
+	split(TreeMap, Key, T1, T2), 
+	merge(T1, Node, Tmp),
+	merge(Tmp, T2, Result).
 
-map_put(TreeMap, Key, Value, Result) :- 
-	tree_add(TreeMap, Key, Value, NewTree1, NewTree2), 
-	create_node(NewTree1, NewTree2, null, Result).
+set_node(node(Key, _, P, L, R), Key, Value, node(Key, Value, P, L, R)).
 
-tree_set(leaf(Key, V), Key, Value, leaf(Key, Value)).
+set_node(node(K, V, P, L, R), Key, Value, Node) :-
+	Key < K, 
+	!, set_node(L, Key, Value, NewL), 
+	Node = node(K, V, P, NewL, R).
 
-tree_set(node2(Max, Key1, Son1, Son2), Key, Value, NewNode) :- 
-	Key1 < Key, !, 
-	tree_set(Son2, Key, Value, NewSon2), 
-	create_node(Son1, NewSon2, null, NewNode).
-	
-tree_set(node2(Max, Key1, Son1, Son2), Key, Value, NewNode) :- 
-	tree_set(Son1, Key, Value, NewSon1), 
-	create_node(NewSon1, Son2, null, NewNode).
-
-tree_set(node3(Max, Key1, Key2, Son1, Son2, Son3), Key, Value, NewNode) :- 
-	Key2 < Key, !, 
-	tree_set(Son3, Key, Value, NewSon3), 
-	create_node(Son1, Son2, NewSon3, NewNode).
-	
-tree_set(node3(Max, Key1, Key2, Son1, Son2, Son3), Key, Value, NewNode) :- 
-	Key1 < Key, !, 
-	tree_set(Son2, Key, Value, NewSon2), 
-	create_node(Son1, NewSon2, Son3, NewNode).
-	
-tree_set(node3(Max, Key1, Key2, Son1, Son2, Son3), Key, Value, NewNode) :- 
-	tree_set(Son1, Key, Value, NewSon1), 
-	create_node(NewSon1, Son2, Son3, NewNode).
-
-tree_add(null, Key, Value, leaf(Key, Value), null).
-
-tree_add(leaf(K, V), Key, Value, leaf(Key, Value), leaf(K, V)) :- Key < K, !.
-tree_add(leaf(K, V), Key, Value, leaf(K, V), leaf(Key, Value)).
-
-tree_add(node2(Max, Key1, Son1, Son2), Key, Value, NewNode, null) :-
-	Key1 < Key, !, 
-	tree_add(Son2, Key, Value, NewSon2, NewSon3),
-	create_node(Son1, NewSon2, NewSon3, NewNode).
-
-tree_add(node2(Max, Key1, Son1, Son2), Key, Value, NewNode, null) :-
-	tree_add(Son1, Key, Value, NewSon1, NewSon2),
-	create_node(NewSon1, NewSon2, Son2, NewNode).
-
-tree_add(node3(Max, Key1, Key2, Son1, Son2, Son3), Key, Value, NewNode1, NewNode2) :- 
-	Key2 < Key, 
-	tree_add(Son3, Key, Value, NewSon3, NewSon4), 
-	!, create_node(Son1, Son2, NewSon3, NewSon4, NewNode1, NewNode2).
-
-tree_add(node3(Max, Key1, Key2, Son1, Son2, Son3), Key, Value, NewNode1, NewNode2) :- 
-	Key1 < Key, 
-	tree_add(Son2, Key, Value, NewSon2, NewSon3), 
-	!, create_node(Son1, NewSon2, NewSon3, Son3, NewNode1, NewNode2).
-
-tree_add(node3(Max, Key1, Key2, Son1, Son2, Son3), Key, Value, NewNode1, NewNode2) :- 
-	tree_add(Son1, Key, Value, NewSon1, NewSon2), 
-	create_node(NewSon1, NewSon2, Son2, Son3, NewNode1, NewNode2).
+set_node(node(K, V, P, L, R), Key, Value, Node) :-
+	set_node(R, Key, Value, NewR), 
+	Node = node(K, V, P, L, NewR).
 
 
-% Map build (unsorted)
-map_build(ListMap, TreeMap) :- 
-	not sorted(ListMap), 
-	!, build_loop(ListMap, null, TreeMap).
+% Map build
+map_build(ListMap, TreeMap) :- build_loop(ListMap, null, TreeMap).
 
 build_loop([], TreeMap, TreeMap).
 
-build_loop([(Key, Value) | Tail], Tree, TreeMap) :- 
-	map_put(Tree, Key, Value, Tree1),
-	build_loop(Tail, Tree1, TreeMap).
+build_loop([(Key, Value) | Tail], T, TreeMap) :- 
+	map_put(T, Key, Value, T1),
+	build_loop(Tail, T1, TreeMap).
 
 
 % Map remove
 map_remove(TreeMap, Key, TreeMap) :- not map_get(TreeMap, Key, _), !.
-map_remove(TreeMap, Key, Result) :- 
-	.
 
+map_remove(TreeMap, Key, Result) :-
+	split(TreeMap, Key, Tmp, T2),
+	Key1 is Key - 1,
+	split(Tmp, Key1, T1, _),
+	merge(T1, T2, Result).
