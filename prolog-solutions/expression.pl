b758746a220d00b1471a872b2acff0f777582c31
Vergil <kolesov645@gmail.com>
2021-06-03 11:55:11 +0300

    Hw-14 base improved

--- prolog-solutions/expression.pl
+++ prolog-solutions/expression.pl
@@ -33,8 +33,10 @@ evaluate(operation(Op, A, B), VarMap, R) :-
 	operation(Op, RA, RB, R).
 
 
-% If -> do
+% If nonvar(V) then do(T)
 if_nonvar_do(V, T) :- (nonvar(V) -> call(T) ; true).
+
+% If nonvar(V) then do(T) else do(G)
 if_nonvar_do(V, T, G) :- (nonvar(V) -> call(T) ; call(G)).
 
 
@@ -49,57 +51,44 @@ oper_p(op_divide)   --> ['/'].
 % Expression parser
 parser(Expr) --> 
 	{ if_nonvar_do(Expr, C = []) },
-	expr_p(Expr), whitespace_p(C).
+	expr_p(Expr), ws_p(C).
 
 expr_p(variable(Name)) -->
-	{ if_nonvar_do(Name, C = []) },
-	whitespace_p(C),
-	[Name],
-	{ member(Name, ['x', 'y', 'z']) }.
+	{ if_nonvar_do(Name, C = [], member(Name, ['x', 'y', 'z'])) },
+	ws_p(C), [Name].
 
 expr_p(const(Value)) -->
 	{ if_nonvar_do(Value, (C = [], number_chars(Value, Chars))) },
-	whitespace_p(C),
-	float_p(Chars),
-	{ Chars = [_ | _], number_chars(Value, Chars) }.
+	ws_p(C), float_p(Chars),
+	{ number_chars(Value, Chars) }.
 
 expr_p(operation(Op, A)) -->
 	{ if_nonvar_do(Op, (C1 = [], C2 = [], C3 = [])) }, 
-	whitespace_p(C1), oper_p(Op), whitespace_p(C2), ['('], expr_p(A), whitespace_p(C3), [')'].
+	ws_p(C1), oper_p(Op), ws_p(C2), ['('], expr_p(A), ws_p(C3), [')'].
 
 expr_p(operation(Op, A, B)) --> 
 	{ if_nonvar_do(Op, (C1 = [], C2 = [' '], C3 = [' '], C4 = []), C3 = []) },
-	whitespace_p(C1), ['('], expr_p(A), whitespace_p(C2), oper_p(Op), whitespace_p(C3), expr_p(B), whitespace_p(C4), [')'].
+	ws_p(C1), ['('], expr_p(A), ws_p(C2), oper_p(Op), ws_p(C3), expr_p(B), ws_p(C4), [')'].
 
-integer_p([]) --> [].
-integer_p([H | T]) --> 
-  { member(H, ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])},
-  [H], 
-  integer_p(T).
+uint_p([H]) --> 
+	{ member(H, ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])}, 
+	[H].
+uint_p([H | T]) --> 
+	{ member(H, ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])},
+  [H], uint_p(T).
+
+int_p(['-' | T]) --> ['-'], uint_p(T).
+int_p(Chars) --> uint_p(Chars).
 
 float_p(Chars) --> 
 	{ if_nonvar_do(Chars, append(Chars1, ['.' | Chars2], Chars)) },
-	integer_p(Chars1),
-	{ Chars1 = [_ | _] },
-	['.'],
-	integer_p(Chars2),
-	{ Chars2 = [_ | _], append(Chars1, ['.' | Chars2], Chars) }.
+	int_p(Chars1), ['.'], uint_p(Chars2),
+	{ append(Chars1, ['.' | Chars2], Chars) }.
 
-float_p(Chars) --> 
-	{ if_nonvar_do(Chars, append(['-' | Chars1], ['.' | Chars2], Chars)) },
-	['-'],
-	integer_p(Chars1),
-	{ Chars1 = [_ | _] },
-	['.'],
-	integer_p(Chars2),
-	{ Chars2 = [_ | _], append(['-' | Chars1], ['.' | Chars2], Chars) }.
-
-whitespace_p([]) --> [].
-whitespace_p([' ' | T]) -->
-	[' '],
-	whitespace_p(T).
+ws_p([]) --> [].
+ws_p([' ' | T]) --> [' '], ws_p(T).
 
 
 % Infix parser
 infix_str(Expr, Str) :- ground(Expr), !, phrase(parser(Expr), Chars), atom_chars(Str, Chars).
-infix_str(Expr, Str) :-   atom(Str), !, atom_chars(Str, Chars), phrase(parser(Expr), Chars).
+infix_str(Expr, Str) :-    atom(Str), !, atom_chars(Str, Chars), phrase(parser(Expr), Chars).
