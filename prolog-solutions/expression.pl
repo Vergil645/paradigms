76f590fc99b0a001234be9f3dd2f807937d200a7
Vergil <kolesov645@gmail.com>
2021-06-04 15:41:06 +0300

    Hw-14 VarBoolean added

--- prolog-solutions/expression.pl
+++ prolog-solutions/expression.pl
@@ -7,6 +7,17 @@ lookup(K, [(K, V) | _], V).
 lookup(K, [_ | T], V) :- lookup(K, T, V).
 
 
+% Lower case
+lower_case('x', 'x').
+lower_case('X', 'x').
+
+lower_case('y', 'y').
+lower_case('Y', 'y').
+
+lower_case('z', 'z').
+lower_case('Z', 'z').
+
+
 % Unary operations
 operation(op_negate, X, R) :- R is -X.
 
@@ -17,11 +28,25 @@ operation(op_subtract, X, Y, R) :- R is X - Y.
 operation(op_multiply, X, Y, R) :- R is X * Y.
 operation(op_divide,   X, Y, R) :- R is X / Y.
 
+operation(op_and, X, Y, 1) :- X > 0, Y > 0, !.
+operation(op_and, X, Y, 0).
+
+operation(op_or,  X, Y, 0) :- X =< 0, Y =< 0, !.
+operation(op_or,  X, Y, 1).
+
+operation(op_xor, X, Y, 1) :- X > 0, Y =< 0, !.
+operation(op_xor, X, Y, 1) :- X =< 0, Y > 0, !.
+operation(op_xor, X, Y, 0).
+	
+
 
 % Evaluate
 evaluate(const(Value), _, Value).
 
-evaluate(variable(Name), VarMap, R) :- lookup(Name, VarMap, R).
+evaluate(variable(Name), VarMap, R) :- 
+	atom_chars(Name, [H | _]),
+	lower_case(H, H1),
+	lookup(H1, VarMap, R).
 
 evaluate(operation(Op, A), VarMap, R) :-
 	evaluate(A, VarMap, RA),
@@ -46,6 +71,9 @@ oper_p(op_add)      --> ['+'].
 oper_p(op_subtract) --> ['-'].
 oper_p(op_multiply) --> ['*'].
 oper_p(op_divide)   --> ['/'].
+oper_p(op_and)      --> ['&', '&'].
+oper_p(op_or)       --> ['|', '|'].
+oper_p(op_xor)      --> ['^', '^'].
 
 
 % Expression parser
@@ -54,8 +82,16 @@ parser(Expr) -->
 	expr_p(Expr), ws_p(C).
 
 expr_p(variable(Name)) -->
-	{ if_nonvar_do(Name, C = [], member(Name, ['x', 'y', 'z'])) },
-	ws_p(C), [Name].
+	{ if_nonvar_do(Name, (C = [], atom_chars(Name, Chars))) },
+	ws_p(C), var_p(Chars), 
+	{ atom_chars(Name, Chars) }.
+
+var_p([H]) --> 
+	{ member(H, ['x', 'y', 'z', 'X', 'Y', 'Z']) },
+	[H].
+var_p([H | T]) -->
+	{ member(H, ['x', 'y', 'z', 'X', 'Y', 'Z']) },
+	[H], var_p(T).
 
 expr_p(const(Value)) -->
 	{ if_nonvar_do(Value, (C = [], number_chars(Value, Chars))) },
