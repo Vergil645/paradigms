80d3a5bf054c923f8accb43639f5f454eda33634
Vergil <kolesov645@gmail.com>
2021-06-04 16:04:25 +0300

    Hw-14 fixed

--- prolog-solutions/expression.pl
+++ prolog-solutions/expression.pl
@@ -7,18 +7,6 @@ lookup(K, [(K, V) | _], V).
 lookup(K, [_ | T], V) :- lookup(K, T, V).
 
 
-% Lower case
-% :NOTE: lookup(N, [('x', 'x'), ('X', 'x'), ...], R).
-lower_case('x', 'x').
-lower_case('X', 'x').
-
-lower_case('y', 'y').
-lower_case('Y', 'y').
-
-lower_case('z', 'z').
-lower_case('Z', 'z').
-
-
 % Unary operations
 operation(op_negate, X, R) :- R is -X.
 
@@ -46,7 +34,7 @@ evaluate(const(Value), _, Value).
 
 evaluate(variable(Name), VarMap, R) :- 
 	atom_chars(Name, [H | _]),
-	lower_case(H, H1),
+	lookup(H, [('x', 'x'), ('X', 'x'), ('y', 'y'), ('Y', 'y'), ('z', 'z'), ('Z', 'z')], H1),
 	lookup(H1, VarMap, R).
 
 evaluate(operation(Op, A), VarMap, R) :-
@@ -85,11 +73,9 @@ parser(Expr) -->
 expr_p(variable(Name)) -->
 	{ if_nonvar_do(Name, (C = [], atom_chars(Name, Chars))) },
 	ws_p(C), var_p(Chars), 
-	{ atom_chars(Name, Chars) }.
+	{ Chars = [_ | _], atom_chars(Name, Chars) }.
 
-var_p([H]) --> 
-	{ member(H, ['x', 'y', 'z', 'X', 'Y', 'Z']) },
-	[H].
+var_p([]) --> [].
 var_p([H | T]) -->
 	{ member(H, ['x', 'y', 'z', 'X', 'Y', 'Z']) },
 	[H], var_p(T).
@@ -107,20 +93,18 @@ expr_p(operation(Op, A, B)) -->
 	{ if_nonvar_do(Op, (C1 = [], C2 = [' '], C3 = [' '], C4 = []), C3 = []) },
 	ws_p(C1), ['('], expr_p(A), ws_p(C2), oper_p(Op), ws_p(C3), expr_p(B), ws_p(C4), [')'].
 
-uint_p([H]) --> 
-	{ member(H, ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])}, 
-	[H].
+uint_p([]) --> [].
 uint_p([H | T]) --> 
 	{ member(H, ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])},
   [H], uint_p(T).
 
-int_p(['-' | T]) --> ['-'], uint_p(T).
-int_p(Chars) --> uint_p(Chars).
+int_p(['-', H | T]) --> ['-'], uint_p([H | T]).
+int_p([H | T]) --> uint_p([H | T]).
 
 float_p(Chars) --> 
 	{ if_nonvar_do(Chars, append(Chars1, ['.' | Chars2], Chars)) },
 	int_p(Chars1), ['.'], uint_p(Chars2),
-	{ append(Chars1, ['.' | Chars2], Chars) }.
+	{ Chars2 = [_ | _], append(Chars1, ['.' | Chars2], Chars) }.
 
 ws_p([]) --> [].
 ws_p([' ' | T]) --> [' '], ws_p(T).
